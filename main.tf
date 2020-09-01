ba7cc8511bde131f1637c0e87fb2d86f6b7d3646
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-09-01 17:53:16 +0100

    removed data lookups

--- main.tf
+++ main.tf
@@ -12,35 +12,35 @@ data "azurerm_image" "win_image" {
 }
 
 # Get Resource Group
-data "azurerm_resource_group" "vm_rg" {
-  name = var.vm_rg
-}
+#data "azurerm_resource_group" "vm_rg" {
+#  name = var.vm_rg
+#}
 
 # Get Azure vNET Details
-data "azurerm_virtual_network" "vm_vnet" {
-  name                = var.vm_vnet_name
-  resource_group_name = var.vnet_rg_name
-}
+#data "azurerm_virtual_network" "vm_vnet" {
+#  name                = var.vm_vnet_name
+#  resource_group_name = var.vnet_rg_name
+#}
 
 # Get Azure Subnet details
-data "azurerm_subnet" "vm_subnet" {
-  name                 = var.vm_subnet_name
-  virtual_network_name = data.azurerm_virtual_network.vm_vnet.name
-  resource_group_name  = var.vnet_rg_name
-}
+#data "azurerm_subnet" "vm_subnet" {
+#  name                 = var.vm_subnet_name
+#  virtual_network_name = data.azurerm_virtual_network.vm_vnet.name
+#  resource_group_name  = var.vnet_rg_name
+#}
 
 # CREATE RESOURCES
 
 # Create VM Network Interfaces
 resource "azurerm_network_interface" "vm_nic" {
   name                = "${var.vm_name}-nic"
-  location            = data.azurerm_resource_group.vm_rg.location
-  resource_group_name = data.azurerm_resource_group.vm_rg.name
+  location            = var.location #data.azurerm_resource_group.vm_rg.location
+  resource_group_name = var.rg_name  # data.azurerm_resource_group.vm_rg.name
   tags                = var.tags
 
   ip_configuration {
     name                          = "${var.vm_name}-ip"
-    subnet_id                     = data.azurerm_subnet.vm_subnet.id
+    subnet_id                     = var.subnet_id # data.azurerm_subnet.vm_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -51,8 +51,8 @@ resource "azurerm_windows_virtual_machine" "win_vm" {
   # count if true conditional
   count               = var.win_image_name ? 1 : 0
   name                = var.vm_name
-  location            = data.azurerm_resource_group.vm_rg.location
-  resource_group_name = data.azurerm_resource_group.vm_rg.name
+  location            = var.location # data.azurerm_resource_group.vm_rg.location
+  resource_group_name = var.rg_name  # data.azurerm_resource_group.vm_rg.name
   size                = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
@@ -85,8 +85,8 @@ resource "azurerm_windows_virtual_machine" "win_vm" {
 resource "azurerm_windows_virtual_machine" "vm" {
   count                    = var.win_image_name ? 0 : 1
   name                     = var.vm_name
-  location                 = data.azurerm_resource_group.vm_rg.location
-  resource_group_name      = data.azurerm_resource_group.vm_rg.name
+  location                 = var.location # data.azurerm_resource_group.vm_rg.location
+  resource_group_name      = var.rg_name  # data.azurerm_resource_group.vm_rg.name
   size                     = var.vm_size
   admin_username           = var.admin_username
   admin_password           = var.admin_password
