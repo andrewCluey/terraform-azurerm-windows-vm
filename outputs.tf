d9ec938cb2bb35858d16d11e36224026eba88e60
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-09-02 15:55:52 +0100

    added private ip address output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "priv_ip_appdress" {
+output "priv_ip_address" {
   description = "The IP Address assigned to the main VM NIC"
   value       = azurerm_network_interface.vm_nic.private_ip_address
 }
