76d494c26eddd555ec7fc5752191791394ac58e7
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-09-21 12:00:45 +0100

    added network security group creation and association

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "azurerm_network_interface" "vm_nic" {
   }
 }
 
+
 # If 'is_custom_image' = true. Create Custom VM
 resource "azurerm_windows_virtual_machine" "win_vm" {
   count               = var.is_custom_image ? 1 : 0
@@ -43,6 +44,7 @@ resource "azurerm_windows_virtual_machine" "win_vm" {
     type = "SystemAssigned"
   }
 }
+
 
 # If 'is_custom_image' = false. Create VM from standard image. DEFAULT Windows Server 2019 Datacenter.
 resource "azurerm_windows_virtual_machine" "vm" {
@@ -84,3 +86,26 @@ resource "azurerm_windows_virtual_machine" "vm" {
   }
 }
 
+
+##### Network Security Groups #####
+
+# Set Network Security Group on the VM Interface.
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = var.rg_name
+}
+
+# If custom image = TRUE
+resource "azurerm_network_interface_security_group_association" "custom_nsg_association" {
+  count                     = var.is_custom_image ? 1 : 0
+  network_interface_id      = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+# If custom image = FALSE
+resource "azurerm_network_interface_security_group_association" "custom_nsg_association" {
+  count                     = var.is_custom_image ? 0 : 1
+  network_interface_id      = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,13 @@ output "priv_ip_address" {
   value       = azurerm_network_interface.vm_nic.private_ip_address
 }
 
+output "nic_id" {
+  description = "the IP Address of the VM Network Interface"
+  value       = azurerm_network_interface.vm_nic.id
+}
+
+output "vm_nsg_id" {
+  description = "The ID of the Network Secyurity group that has been assigned to the VM"
+  value       = azurerm_network_security_group.vm_nsg.id
+}
 
