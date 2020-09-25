e48e83af174ed4245a13849c8f643216fbeaaa15
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-09-25 16:45:45 +0100

    removed outputs

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,3 @@ output "nic_id" {
   description = "the IP Address of the VM Network Interface"
   value       = azurerm_network_interface.vm_nic.id
 }
-
-output "vm_nsg_id" {
-  description = "The ID of the Network Secyurity group that has been assigned to the VM"
-  value       = azurerm_network_security_group.vm_nsg.id
-}
-
-output "vm_nsg_name" {
-  description = "The name of the Network Secyurity group that has been assigned to the VM"
-  value       = azurerm_network_security_group.vm_nsg.name
-}
